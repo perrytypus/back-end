47352101396480ccc2f30b72ef47fc7063fd7824
perrytypus <ian.bortoli.santos@escola.pr.gov.br>
2026-03-09 08:58:47 -0300

    Create con003.py

---
+++ conteúdo/con003.py
@@ -0,0 +1,10 @@
+n = input('digite algo')
+print(n.isnumeric())#para perguntar se é um número
+print(n.isalpha())#para perguntar se é letra
+print(n.isalnum())#para perguntar se é letra ou numero
+print(n.isupper())#para perguntar se é só com letras
+print(n.isdecimal())#para verificar se é decimal
+print(n.isdigit())#para verificar se é um digito
+print(n.islower())#para veificar se é apenas letras minusculas
+print(n.isidentifier())#para identificar se é um identificador válido
+print(n.isspace())#para verificar se tem espaço em branco
