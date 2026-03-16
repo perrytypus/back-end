ef77a104a4ca86bd6946fcdbe6cd04a3683647db
perrytypus <ian.bortoli.santos@escola.pr.gov.br>
2026-03-16 10:56:30 -0300

    Create cont06.py

---
+++ conteúdo/cont06.py
@@ -0,0 +1,14 @@
+#alinhamento de máscara 
+#podemos alinhar o nome entro do número de caracteres que desejamos 
+#alinhamento para a direita
+#ex: nome=input("qual é o seu nome? ")
+#   print('prazer em conhecelo{:>20}!'.format(nome))
+#alinhamento para a esquerda 
+#ex: nome=input("qual é o seu nome? ")
+#   print('prazer em conhecelo{:<20}!'.format(nome))
+#alinhamento no centro
+#ex: nome=input("qual é o seu nome? ")
+#   print('prazer em conhecelo{:^20}!'.format(nome))
+#podemos escrever um resultado na quantidade de caracteres especifica aocolocar o valor dentro da mascara após escrever
+#ex: nome=input("qual é o seu nome? ")
+#   print('prazer em conhecelo{:20}!'.format(nome))
