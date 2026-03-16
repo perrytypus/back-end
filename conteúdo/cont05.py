5db0b25bb752869155854da84d66b38e2746bf9a
perrytypus <ian.bortoli.santos@escola.pr.gov.br>
2026-03-16 10:47:15 -0300

    Create cont05.py

---
+++ conteúdo/cont05.py
@@ -0,0 +1,9 @@
+#5+3*2==
+#3*5+4**2=
+#3*(5+4)**2=
+#o python é utilizado em calculos científicos é o tamanho da memória ou seja conseguimos chegar a números extremamente grandes
+#outros operadores 
+#pra expnenciação podemos usar o pow(x,y) onde o numero x é elevado ao número y
+#para raiz quadrada podemo usar o **(1/2). elevando o número a 1/2 nos obtemos a raiz quadrada. para a raiz cubica **(1/3) e assim por diante 
+#podemos usar os operadores em string
+#ex:'='5 vai ficar '====='
