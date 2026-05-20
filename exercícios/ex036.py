ef9fd57c8285560a842debac8040052f87721916
perrytypus <ian.bortoli.santos@escola.pr.gov.br>
2026-05-20 10:59:36 -0300

    Update ex036.py

--- exercícios/ex036.py
+++ exercícios/ex036.py
@@ -3,7 +3,7 @@ if nome == "Gustavo":
     print("que nome bonito")
 elif nome == "Paulo" or nome == "Maria" or nome == "Pedro":
     print("Seu nome é bem popular no Brasil")
-elif nome in "Ana Claudia Jéssica Juliana":
+elif nome in ["Ana", "Claudia", "Jéssica", "Juliana"]:
     print("que belo nome feminino")
 else:
     print("seu nome é bem normal")
