6a65224442997b0dc9f591ebedef99a6c66a793d
perrytypus <ian.bortoli.santos@escola.pr.gov.br>
2026-05-20 10:42:09 -0300

    Create ex035.py

---
+++ exercícios/ex035.py
@@ -0,0 +1,10 @@
+print("-=-" * 10)
+print("Analisador de triángulos")
+print("-=-"* 10)
+a float(input("Primeiro segmento"))
+bfloat(input("Segundo segmento"))
+c float(input("Terceiro segmento"))
+if a<b+c and b<a+c and c<a+b:
+  print("Os segmentos acima podem formar um triangulo")
+else:
+  print("os segmentos acima não podem formar um triangulo")
