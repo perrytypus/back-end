78a9c26dd2c46b8041252034800bc93b17a8ccf6
perrytypus <ian.bortoli.santos@escola.pr.gov.br>
2026-05-20 10:55:22 -0300

    Create ex036.py

---
+++ exercícios/ex036.py
@@ -0,0 +1,10 @@
+nome=str(input("qual é o seu nome? "))
+if nome == "Gustavo":
+    print("que nome bonito")
+elif nome == "Paulo" or nome == "Maria" or nome == "Pedro":
+    print("Seu nome é bem popular no Brasil")
+elif nome in "Ana Claudia Jéssica Juliana":
+    print("que belo nome feminino")
+else:
+    print("seu nome é bem normal")
+    print("tenha um bom dia {}".format(nome))
