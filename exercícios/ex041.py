25d2fd7ec2df4473df32117920d9d64666c83b7d
perrytypus <ian.bortoli.santos@escola.pr.gov.br>
2026-08-24 10:35:33 -0300

    Create ex041.py

---
+++ exercícios/ex041.py
@@ -0,0 +1,12 @@
+print("-=-" * 10)
+print("Analisador de triângulos")
+print("-=-" * 10)
+= float(input("Primeiro segmento "))
+= float(input("Segundo segmento "))
+= float(input("Terceiro segmento"))
+if a<b+c and b<a+c and c<a+b and a== b == c:
+print("Os segmentos acima podem formar um triangulo Equilatero")
+elif a<b+c and b<a+c and c<a+b and a== b or a== c or b==c:
+print("Os segmentos acima podem formar um triangulo Isósceles")
+elif a<b+c and b<a+c and c<a+b and a!= b and a!= c and b!=c:
+print("Os segmentos acima podem formar um triangulo Escaleno")
